d40b8aab431bd71bc719ae3ec3f1d7fd3ddd3625
AMBarbosa <ana.marcia.barbosa@gmail.com>
2024-09-19 15:42:05 +0100

    improve 'region' arg description; add cloglog ref

--- R/getPredictions.R
+++ R/getPredictions.R
@@ -2,16 +2,19 @@
 #'
 #' @param rasts (multi-layer) SpatRaster containing the variables (with the same names) used in the models
 #' @param mods list of [maxnet] model objects, output of [getModels()]
-#' @param region optional SpatExtent or SpatVector polygon delimiting the region of 'rasts' within which to compute the predictions. The default is NULL, to use the entire extent of 'rasts'. Note that predictions may be unreliable outside the 'region' used for [getModels()]
-#' @param type character value to pass to [predict()] indicating the type of response to compute. Can be "cloglog" (the default), "logistic", "exponential" or "link"
+#' @param region optional SpatExtent or SpatVector polygon delimiting the region of 'rasts' within which to compute the predictions. The default is NULL, to use the entire extent of 'rasts'. Note that predictions may be unreliable outside the 'region' used for [getModels()], as they are extrapolating beyond the analysed conditions.
+#' @param type character value to pass to [predict()] indicating the type of response to compute. Can be "cloglog" (the default and currently recommended), "logistic" (previously but no longer recommended) (Phillips et al., 2017), "exponential", or "link"
 #' @param clamp logical value to pass to [predict()] indicating whether predictors and features should be restricted to the range seen during model training. Default TRUE
 #' @param file optional file name (including path, not including extension) if you want the prediction rasters to be saved on disk. If 'file' already exists in the working directory, the rasters are imported from there.
 #' @param verbosity integer value indicating the amount of messages to display. The default is 2, for the maximum number of messages available.
 #'
-#' @return multi-layer SpatRaster with the predicted values from the variables for each year
+#' @return multi-layer SpatRaster with the predicted values for each year
 #' @author A. Marcia Barbosa
 #' @export
 #' @importFrom terra crop predict rast writeRaster
+#' @references
+#' Phillips, S.J., Anderson, R.P., Dudik, M., Schapire, R.E., Blair, M.E., 2017. Opening the black box: an open-source release of Maxent. Ecography 40, 887-893. https://doi.org/10.1111/ecog.03049
+
 #'
 #' @examples
 
