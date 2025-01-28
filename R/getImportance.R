3f0c53355904e02c75d7a78dec0a5add953eb49a
AMBarbosa <ana.marcia.barbosa@gmail.com>
2025-01-28 19:12:43 +0000

    now with RMSE

--- R/getImportance.R
+++ R/getImportance.R
@@ -1,12 +1,12 @@
 #' Get variable importance
 #'
-#' This function computes the permutation importance of each variable in each model, by shuffling each variable in turn (a given number of times) and computing the mean squared difference between the actual model predictions and those obtained with the shuffled variable.
+#' This function computes the permutation importance of each variable in each model, by shuffling each variable in turn (a given number of times) and computing the root mean squared difference between the actual model predictions and those obtained with the shuffled variable. Note that "importance" is a vague concept which can be measured in many other different ways.
 #'
 #' @param mods output of [getModels()].
 #' @param nper integer value (default 10; increase for more accurate but computationally intensive results) indicating the number of permutations for shuffling each variable.
 #' @param verbosity integer value indicating the amount of messages to display. The default is 2, for the maximum number of messages available.
 #'
-#' @return A data frame with the permutation importance of each variable in each replicate for each year, along with the cross-replicate mean and standard deviation within each year.
+#' @return A data frame with the permutation importance of each variable in each model replicate for each year, along with the cross-replicate mean and standard deviation.
 #'
 #' @seealso \code{varImportance} in package \pkg{predicts}; \code{bm_VariablesImportance} in package \pkg{biomod2}
 #'
@@ -41,6 +41,7 @@ getImportance <- function(mods, nper = 10, verbosity = 2) {
     names(varimps[[y]]) <- names(models[[y]])
 
     for (r in 1:n_reps){
+      # varimps[[y]][[r]] <- predicts::varImportance(models[[y]][[r]], y = mods$data$presence, x = dat, n = nper, stat = "RMSE", type = "cloglog")
       varimps[[y]][[r]] <- varImpor(models[[y]][[r]], data = dat, nper = nper)
     }  # end for r
 
@@ -48,7 +49,7 @@ getImportance <- function(mods, nper = 10, verbosity = 2) {
   }  # end for y
 
   varimps <- do.call(rbind.data.frame, varimps)
-  varimps$mean <- rowMeans(varimps)
+  varimps$mean <- apply(varimps, 1, mean)
   varimps$sd <- apply(varimps, 1, sd)
 
   splits <- strsplit(rownames(varimps), "\\.")
@@ -62,18 +63,19 @@ getImportance <- function(mods, nper = 10, verbosity = 2) {
 
 
 varImpor <- function(model, data, nper) {
-  original_predictions <- predict(model, data)
+  original_predictions <- as.vector(predict(model, data, type = "cloglog"))
   importance_scores <- numeric(ncol(data))
 
   for (i in seq_along(importance_scores)) {
     permuted_scores <- numeric(nper)
 
-    for (j in 1:nper) {
+    for (p in 1:nper) {
       permuted_data <- data
       permuted_data[, i] <- sample(permuted_data[, i])
-      permuted_predictions <- predict(model, permuted_data)
-      permuted_scores[j] <- mean((original_predictions - permuted_predictions)^2)
-    }
+      permuted_predictions <- as.vector(predict(model, permuted_data, type = "cloglog"))
+      # permuted_scores[p] <- mean((original_predictions - permuted_predictions)^2)
+      permuted_scores[p] <- mean(modEvA::errorMeasures(obs = original_predictions, pred = permuted_predictions)$RMSE)
+      }
 
     importance_scores[i] <- mean(permuted_scores)
   }
